4add088db55aa7f087293d5f633c4597899fbb76
tushitamallik <56465326+tushitamallik@users.noreply.github.com>
2023-07-05 14:48:56 +0530

    Create String Validators.py

---
+++ Python/HackerRank/String Validators.py
@@ -0,0 +1,12 @@
+'''
+Python has built-in string validation methods for basic data. 
+It can check if a string is composed of alphabetical characters, alphanumeric characters, digits, etc.
+'''
+======================================================================================================
+if __name__ == '__main__':
+    s = input()
+    print(any(i.isalnum() for i in s))
+    print(any(i.isalpha() for i in s))
+    print(any(i.isdigit() for i in s))
+    print(any(i.islower() for i in s))
+    print(any(i.isupper() for i in s))
