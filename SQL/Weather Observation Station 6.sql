42d19951f98ba2a79c772f9b8a48ab85853dd4df
tushitamallik <56465326+tushitamallik@users.noreply.github.com>
2024-08-04 10:44:20 +0530

    Update Weather Observation Station 6.sql

--- SQL/Weather Observation Station 6.sql
+++ SQL/Weather Observation Station 6.sql
@@ -1,5 +1,5 @@
 /*
-  Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. 
+  Query the list of CITY names starting with vowels (a, e, i, o, u) from STATION. 
      Your result cannot contain duplicates.
 */
-SELECT DISTINCT city FROM station WHERE city LIKE "%a" OR city LIKE "%e" OR city LIKE "%i" OR city LIKE "%o" OR city LIKE "%u";
+SELECT DISTINCT city FROM station WHERE city LIKE "a%" OR city LIKE "e%" OR city LIKE "i%" OR city LIKE "o%" OR city LIKE "u%";
